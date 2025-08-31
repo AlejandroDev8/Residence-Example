fffeb1546f873fda29ccd61549a16282ac913956
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-08-31 17:49:21 -0600

    Add ListLocalities page and comment out CreateAction in ListStates page

--- app/Filament/Resources/LocalityResource/Pages/ListLocalities.php
+++ app/Filament/Resources/LocalityResource/Pages/ListLocalities.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace App\Filament\Resources\StateResource\Pages;
+namespace App\Filament\Resources\LocalityResource\Pages;
 
-use App\Filament\Resources\StateResource;
+use App\Filament\Resources\LocalityResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
-class ListStates extends ListRecords
+class ListLocalities extends ListRecords
 {
-    protected static string $resource = StateResource::class;
+    protected static string $resource = LocalityResource::class;
 
     protected function getHeaderActions(): array
     {
