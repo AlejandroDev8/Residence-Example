8c6b0ebdc63105123eb43f66ea5a1b6a27180a14
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-09-05 00:57:56 -0600

    Refactor MaizeSampleResource form to enhance structure and usability with fieldsets for better organization of sample data

--- app/Filament/Resources/MaizeSampleResource.php
+++ app/Filament/Resources/MaizeSampleResource.php
@@ -9,16 +9,21 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\HasManyRepeater;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
 
 class MaizeSampleResource extends Resource
 {
@@ -35,135 +40,210 @@ class MaizeSampleResource extends Resource
         return $form
             ->schema([
                 Section::make('Datos de la muestra')
+                    ->description('Captura los datos generales de la muestra de maíz.')
                     ->schema([
-                        TextInput::make('code')
-                            ->label('Código')
-                            ->maxLength(50),
+                        Fieldset::make('Identificación')
+                            ->schema([
+                                TextInput::make('code')
+                                    ->label('Código')
+                                    ->required()
+                                    ->maxLength(50),
 
-                        TextInput::make('sample_number')
-                            ->label('N° Muestra')
-                            ->numeric()
-                            ->required(),
+                                TextInput::make('sample_number')
+                                    ->label('N° Muestra')
+                                    ->numeric()
+                                    ->required(),
+                            ]),
 
-                        // Recolector (users)
-                        Select::make('user_id')
-                            ->label('Recolector')
-                            ->relationship('collector', 'name') // MaizeSample::collector()->belongsTo(User::class, 'user_id')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
+                        Fieldset::make('Recolector y Agricultor')
+                            ->schema([
+                                Select::make('user_id')
+                                    ->label('Recolector')
+                                    ->relationship('collector', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required(),
 
-                        // Agricultor (farmers)
-                        Select::make('farmer_id')
-                            ->label('Agricultor')
-                            ->relationship('farmer', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->nullable(),
+                                Select::make('farmer_id')
+                                    ->label('Agricultor')
+                                    ->relationship('farmer', 'name')
+                                    ->required()
+                                    ->searchable()
+                                    ->preload()
+                                    ->nullable(),
+                            ]),
 
-                        // Municipio
-                        Select::make('municipality_id')
-                            ->label('Municipio')
-                            ->relationship('municipality', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->reactive()
-                            ->required(),
+                        Fieldset::make('Ubicación')
+                            ->schema([
+                                Select::make('municipality_id')
+                                    ->label('Municipio')
+                                    ->relationship('municipality', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->reactive()
+                                    ->live()
+                                    ->required()
+                                    ->afterStateUpdated(function (Set $set) {
+                                        $set('locality_id', null);
+                                    }),
 
-                        // Comunidad dependiente del municipio
-                        Select::make('locality_id')
-                            ->label('Comunidad')
-                            // Opciones filtradas por el municipio seleccionado
-                            ->options(fn(Get $get) => ($get('municipality_id'))
-                                ? Locality::query()
-                                ->where('municipality_id', $get('municipality_id'))
-                                ->orderBy('name')
-                                ->pluck('name', 'id')
-                                ->toArray()
-                                : [])
-                            ->searchable()
-                            ->preload()
-                            ->required()
-                            ->disabled(fn(Get $get) => blank($get('municipality_id')))
-                            ->reactive(),
+                                Select::make('locality_id')
+                                    ->label('Comunidad')
+                                    ->options(fn(Get $get): Collection => Locality::query()->where('municipality_id', $get('municipality_id'))->pluck('name', 'id'))
+                                    ->searchable()
+                                    ->preload()
+                                    ->live()
+                                    ->required(),
 
-                        DatePicker::make('collection_date')
-                            ->label('Fecha de colecta'),
+                                TextInput::make('latitude')
+                                    ->label('Latitud (N)')
+                                    ->required()
+                                    ->numeric()
+                                    ->step('0.000001'),
 
-                        TextInput::make('latitude')
-                            ->label('Latitud (N)')
-                            ->numeric()
-                            ->step('0.000001'),
+                                TextInput::make('longitude')
+                                    ->label('Longitud (O)')
+                                    ->required()
+                                    ->numeric()
+                                    ->step('0.000001'),
+                            ]),
 
-                        TextInput::make('longitude')
-                            ->label('Longitud (O)')
-                            ->numeric()
-                            ->step('0.000001'),
+                        Fieldset::make('Detalles de la muestra')
+                            ->schema([
+                                DatePicker::make('collection_date')
+                                    ->format('d-M-Y')
+                                    ->displayFormat('d-M-Y')
+                                    ->timezone('America/Mexico_City')
+                                    ->native(false)
+                                    ->required()
+                                    ->label('Fecha de colecta')
+                                    ->columnSpan(1),
 
-                        TextInput::make('variety_name')
-                            ->label('Nombre de la variedad'),
+                                TextInput::make('variety_name')
+                                    ->label('Nombre de la variedad')
+                                    ->required()
+                                    ->columnSpan(1),
 
-                        Forms\Components\Textarea::make('notes')
-                            ->label('Notas')
-                            ->rows(3),
+                                Textarea::make('notes')
+                                    ->label('Notas')
+                                    ->autosize()
+                                    ->rows(3)
+                                    ->columnSpanFull(),
+                            ])
+                            ->columns([
+                                'sm' => 1,
+                                'md' => 2,
+                            ])
                     ])
                     ->columns(3),
 
                 Section::make('Sub-muestras')
                     ->description('Captura una o varias sub-muestras con sus mediciones.')
                     ->schema([
-                        HasManyRepeater::make('subsamples')
+                        Repeater::make('subsamples')
                             ->label('Sub-muestras')
                             ->relationship('subsamples') // MaizeSample::subsamples()
                             ->defaultItems(1)
-                            ->orderable(false)
                             ->addActionLabel('Agregar sub-muestra')
                             ->schema([
-                                TextInput::make('subsample_number')
-                                    ->label('N° Submuestra')
-                                    ->numeric()
-                                    ->minValue(0)
-                                    ->required(),
+                                Fieldset::make('Identificación')->schema([
+                                    TextInput::make('subsample_number')
+                                        ->label('N° Submuestra')
+                                        ->numeric()
+                                        ->minValue(0)
+                                        ->required(),
+                                ]),
 
                                 // Categóricos
-                                TextInput::make('color_grano')->label('Color de grano')
-                                    ->datalist(['BLANCO', 'AMARILLO', 'ROJO', 'MORADO', 'AZUL', 'NEGRO']),
-                                TextInput::make('color_olote')->label('Color de olote')
-                                    ->datalist(['BLANCO', 'ROSA', 'ROJO', 'CAFÉ']),
-                                TextInput::make('tipo_grano')->label('Tipo de grano')
-                                    ->datalist(['Dentado', 'Duro', 'Reventador', 'Harinoso']),
-                                TextInput::make('forma_corona_grano')->label('Forma de la corona'),
-                                TextInput::make('color_dorsal_grano')->label('Color dorsal'),
-                                TextInput::make('color_endospermo_grano')->label('Color del endospermo'),
-                                TextInput::make('arreglo_hileras_grano')->label('Arreglo de hileras'),
+                                Fieldset::make('Categóricos')
+                                    ->schema([
+                                        Select::make('color_grano')
+                                            ->label('Color de grano')
+                                            ->options([
+                                                'BLANCO',
+                                                'AMARILLO',
+                                                'ROJO',
+                                                'MORADO',
+                                                'AZUL',
+                                                'NEGRO'
+                                            ]),
+                                        Select::make('color_olote')
+                                            ->label('Color de olote')
+                                            ->options([
+                                                'BLANCO',
+                                                'ROSA',
+                                                'ROJO',
+                                                'CAFÉ'
+                                            ]),
+                                        Select::make('tipo_grano')
+                                            ->label('Tipo de grano')
+                                            ->options([
+                                                'Dentado',
+                                                'Duro',
+                                                'Reventador',
+                                                'Harinoso'
+                                            ]),
+                                        TextInput::make('forma_corona_grano')
+                                            ->label('Forma de la corona')
+                                            ->helperText('Puntiaguda, redondeada, etc.'),
+                                        TextInput::make('color_dorsal_grano')
+                                            ->label('Color dorsal')
+                                            ->helperText('Color del lado convexo del grano.'),
+                                        TextInput::make('color_endospermo_grano')
+                                            ->label('Color del endospermo')
+                                            ->helperText('Color del interior del grano.'),
+                                        TextInput::make('arreglo_hileras_grano')
+                                            ->label('Arreglo de hileras')
+                                            ->helperText('Regular, irregular, etc.')
+                                            ->columnSpanFull(),
+                                    ]),
 
                                 // Métricos
-                                TextInput::make('diametro_mazorca_mm')->label('Diámetro mazorca (mm)')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('largo_mazorca_mm')->label('Largo mazorca (mm)')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('peso_mazorca_g')->label('Peso de mazorca (g)')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('peso_grano_50_g')->label('Peso de grano 50 (g)')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('num_hileras')->label('N° de hileras')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('num_granos_por_hilera')->label('N° granos/hilera')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('grosor_grano_mm')->label('Grosor grano (mm)')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('ancho_grano_mm')->label('AGR (mm)')
-                                    ->numeric()->minValue(0),
-                                TextInput::make('longitud_grano_mm')->label('LGR (mm)')
-                                    ->numeric()->minValue(0),
+                                Fieldset::make('Métricas')
+                                    ->schema([
+                                        TextInput::make('diametro_mazorca_mm')
+                                            ->label('Diámetro mazorca (mm)')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('largo_mazorca_mm')
+                                            ->label('Largo mazorca (mm)')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('peso_mazorca_g')
+                                            ->label('Peso de mazorca (g)')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('peso_grano_50_g')
+                                            ->label('Peso de grano 50 (g)')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('num_hileras')
+                                            ->label('N° de hileras')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('num_granos_por_hilera')
+                                            ->label('N° granos/hilera')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('grosor_grano_mm')
+                                            ->label('Grosor grano (mm)')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('ancho_grano_mm')
+                                            ->label('AGR (mm)')
+                                            ->numeric()->minValue(0),
+                                        TextInput::make('longitud_grano_mm')
+                                            ->label('LGR (mm)')
+                                            ->numeric()->minValue(0),
+                                    ])
+                                    ->columns(3),
 
                                 // Se calcula en el modelo si no lo mandas
-                                TextInput::make('indice_lgr_agr')->label('Índice LGR/AGR')
-                                    ->disabled()
-                                    ->helperText('Se calcula automáticamente al guardar si LGR y AGR están capturados.'),
+                                Fieldset::make('Cálculos')
+                                    ->schema([
+                                        TextInput::make('indice_lgr_agr')
+                                            ->label('Índice LGR/AGR')
+                                            ->disabled()
+                                            ->helperText('Se calcula automáticamente al guardar si LGR y AGR están capturados.'),
 
-                                TextInput::make('volumen_grano_50_ml')->label('Volumen 50 semillas (ml)')
-                                    ->numeric()->minValue(0),
+                                        TextInput::make('volumen_grano_50_ml')
+                                            ->label('Volumen 50 semillas (ml)')
+                                            ->numeric()
+                                            ->minValue(0),
+                                    ])
                             ])
                             ->columns(3),
                     ]),
