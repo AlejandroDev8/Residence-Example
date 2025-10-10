6efb6cdad557beb13173661b0207da75fe64c860
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-10-10 12:42:33 -0600

    Add Create, Edit, and List pages for MaizeSample resource

---
+++ app/Filament/Dashboard/Resources/MaizeSampleResource/Pages/CreateMaizeSample.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\MaizeSampleResource\Pages;
+
+use App\Filament\Dashboard\Resources\MaizeSampleResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMaizeSample extends CreateRecord
+{
+    protected static string $resource = MaizeSampleResource::class;
+}
