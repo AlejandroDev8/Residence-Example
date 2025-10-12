c6429fa71a690fa80f48cc9d56731690a84ec00a
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-10-12 13:40:19 -0600

    Add redirect and notification methods to CreateUser and EditUser pages

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
@@ -15,5 +16,18 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title("El usuario {$this->record->name} ha sido actualizado")
+            ->body('Los cambios han sido guardados exitosamente.');
     }
 }
